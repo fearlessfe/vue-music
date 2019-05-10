8333ad0fb599832a942ff12727fff2f47c7beeed
fearlessfe <505380967@qq.com>
2019-05-10 23:24:02 +0800

    优化server代码

--- src/server/server.js
+++ src/server/server.js
@@ -3,9 +3,7 @@ const axios = require('axios')
 
 const app = express()
 
-app.get('/api/getDiscList', function (req, res) {
-  const url = 'https://u.y.qq.com/cgi-bin/musicu.fcg?-=recom9548731252966711'
-
+const handleReq = (req, res, url) => {
   axios.get(url, {
     headers: {
       origin: 'https://y.qq.com',
@@ -17,112 +15,48 @@ app.get('/api/getDiscList', function (req, res) {
   }).catch(err => {
     console.log(err.response.data)
   })
+}
+
+app.get('/api/getDiscList', function (req, res) {
+  const url = 'https://u.y.qq.com/cgi-bin/musicu.fcg?-=recom9548731252966711'
+
+  handleReq(req, res, url)
 })
 
 app.get('/api/getSinger', function (req, res) {
   const url = 'https://u.y.qq.com/cgi-bin/musicu.fcg?-=getUCGI31390744699875195'
 
-  axios.get(url, {
-    headers: {
-      origin: 'https://y.qq.com',
-      referer: 'https://y.qq.com/'
-    },
-    params: req.query
-  }).then(response => {
-    res.json(response.data)
-  }).catch(err => {
-    console.log(err.response.data)
-  })
+  handleReq(req, res, url)
 })
 
 app.get('/api/getSingerDetail', function (req, res) {
   const url = 'https://c.y.qq.com/v8/fcg-bin/fcg_v8_singer_track_cp.fcg'
-  axios.get(url, {
-    headers: {
-      origin: 'https://y.qq.com',
-      referer: 'https://y.qq.com/'
-    },
-    params: req.query
-  }).then(response => {
-    res.json(response.data)
-  }).catch(err => {
-    console.log(err.response.data)
-  })
+  handleReq(req, res, url)
 })
 
 app.get('/api/getVkey', function (req, res) {
   const url = 'https://u.y.qq.com/cgi-bin/musicu.fcg?-=getplaysongvkey722367561740654'
-  axios.get(url, {
-    headers: {
-      origin: 'https://y.qq.com',
-      referer: 'https://y.qq.com/'
-    },
-    params: req.query
-  }).then(response => {
-    res.json(response.data)
-  }).catch(err => {
-    console.log(err.response.data)
-  })
+  handleReq(req, res, url)
 })
 
 app.get('/api/getLyric', function (req, res) {
   const url = 'https://c.y.qq.com/lyric/fcgi-bin/fcg_query_lyric_yqq.fcg?-=jsonp1'
-  axios.get(url, {
-    headers: {
-      origin: 'https://y.qq.com',
-      referer: 'https://y.qq.com/'
-    },
-    params: req.query
-  }).then(response => {
-    res.json(response.data)
-  }).catch(err => {
-    console.log(err.response.data)
-  })
+  handleReq(req, res, url)
 })
 
 app.get('/api/getSongList', function (req, res) {
   const url = 'https://c.y.qq.com/qzone/fcg-bin/fcg_ucc_getcdinfo_byids_cp.fcg'
-  axios.get(url, {
-    headers: {
-      origin: 'https://y.qq.com',
-      referer: 'https://y.qq.com/'
-    },
-    params: req.query
-  }).then(response => {
-    res.json(response.data)
-  }).catch(err => {
-    console.log(err.response.data)
-  })
+  handleReq(req, res, url)
 })
 
 app.get('/api/getTopList', function (req, res) {
   const url = 'https://c.y.qq.com/v8/fcg-bin/fcg_myqq_toplist.fcg'
-  axios.get(url, {
-    headers: {
-      origin: 'https://y.qq.com',
-      referer: 'https://y.qq.com/'
-    },
-    params: req.query
-  }).then(response => {
-    res.json(response.data)
-  }).catch(err => {
-    console.log(err.response.data)
-  })
+  handleReq(req, res, url)
 })
 
 app.get('/api/search', function (req, res) {
   const url = 'https://c.y.qq.com/soso/fcgi-bin/search_for_qq_cp'
-  axios.get(url, {
-    headers: {
-      origin: 'https://y.qq.com',
-      referer: 'https://y.qq.com/'
-    },
-    params: req.query
-  }).then(response => {
-    res.json(response.data)
-  }).catch(err => {
-    console.log(err.response.data)
-  })
+  handleReq(req, res, url)
 })
 
 app.listen(3333, function () {
